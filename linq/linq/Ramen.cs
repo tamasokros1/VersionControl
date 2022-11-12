a50b9727be67ddd6eda949af91314f52995cea88
tamasokros1 <tamas.okros@stud.uni-corvinus.hu>
2022-11-12 09:19:26 +0100

    feladat1

---
+++ linq/linq/Ramen.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linq
+{
+    class Ramen
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Brand { get; set; }
+        public int CountryFK { get; set; }
+        public Country Country { get; set; }
+        public double Rating { get; set; }
+    }
+}
