42d032c9dec82dbe71e46211226579e8020563b8
Tamás Ökrös <tamas.okros@stud.uni-corvinus.hu>
2022-11-19 13:54:39 +0100

    usingExcel

--- UserMaintenance/week04/Form1.cs
+++ UserMaintenance/week04/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Reflection;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace week04
 {
