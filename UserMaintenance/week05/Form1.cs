5bfdd18e7a78f9a405482fef003318f4ceaf4d5c
Tamás Ökrös <tamas.okros@stud.uni-corvinus.hu>
2022-11-26 09:12:59 +0100

    feladat3

--- UserMaintenance/week05/Form1.cs
+++ UserMaintenance/week05/Form1.cs
@@ -3,18 +3,41 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using week05.MnbServiceReference;
 
 namespace week05
 {
     public partial class Form1 : Form
     {
+        
         public Form1()
         {
             InitializeComponent();
+            GetExchangeRates();
         }
+        void GetExchangeRates()
+        {
+            var mnbService = new MNBArfolyamServiceSoapClient();
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+            var response = mnbService.GetExchangeRates(request);
+            var result = response.GetExchangeRatesResult;
+            string exchangeResult = result;
+            File.WriteAllText(@"C:\Users\Tamás\source\repos\VersionControl\UserMaintenance\week05\result.xml", result);
+            string readResult = File.ReadAllText(@"C:\Users\Tamás\source\repos\VersionControl\UserMaintenance\week05\result.xml");
+            Console.WriteLine(readResult);
+
+        }
+
     }
+
 }
