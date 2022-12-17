0888f8d428a4206fe4949c0dbf6921bdc1fa13e1
tamasokros1 <tamas.okros@stud.uni-corvinus.hu>
2022-12-17 13:59:55 +0100

    feladat7

--- UserMaintenance/week06/Form1.cs
+++ UserMaintenance/week06/Form1.cs
@@ -32,5 +32,19 @@ namespace week06
 
             dataGridView2.DataSource = Portfolio;
         }
+        private decimal GetPortfolioValue(DateTime date)
+        {
+            decimal value = 0;
+            foreach (var item in Portfolio)
+            {
+                var last = (from x in Ticks
+                            where item.Index == x.Index.Trim()
+                               && date <= x.TradingDay
+                            select x)
+                            .First();
+                value += (decimal)last.Price * item.Volume;
+            }
+            return value;
+        }
     }
 }
